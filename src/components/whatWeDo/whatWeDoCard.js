fd87d100358270119e8a005741d95d9aa07e0e31
bulish <babickovalibuse@seznam.cz>
2021-06-19 14:40:42 +0200

    responzivita je hotova

--- src/components/whatWeDo/whatWeDoCard.js
+++ src/components/whatWeDo/whatWeDoCard.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import {gsap} from 'gsap';
 
-function WhatWeDoCard({ cards, setCards, card }) {
+function WhatWeDoCard({ card }) {
+	const mouseEnterBtn = (event) => {
+		const myTarget = event.target.children;
+		gsap.to(myTarget, { x: '100%' });
+	};
 
+	const mouseLeaveBtn = (event) => {
+		const myTarget2 = event.target.children;
+		gsap.to(myTarget2, { x: '0%' });
+	};
 	return (
 		<div className="card">
 			<div className="card__subtitle">{card.subtitle}</div>
 			<div className="card__title">{card.title}</div>
 			<div className="card__text">{card.text}</div>
-			<div className="card__btn">
-				<div className="card__btn">{card.button}</div>
-			</div>
+			<div className="card__btn" onMouseEnter={mouseEnterBtn} onMouseLeave={mouseLeaveBtn}>{card.button}
+				<div className="card__btn__hover"></div></div>
 		</div>
 	);
 }
